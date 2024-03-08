c26e7e83931c8a4426c20d07e6cf02b068931149
David Boschmann <davidboschmann@live.de>
2024-03-08 20:41:26 +0100

    fix: physics

--- GuardiansRace/Assets/CarController.cs
+++ GuardiansRace/Assets/CarController.cs
@@ -4,7 +4,7 @@ public class CarController : MonoBehaviour
 {
 
     [SerializeField] float acceleration = 4;
-    [SerializeField] float maxSpeed;
+    [SerializeField] float maxSpeed = 100;
     [SerializeField] float turnFactor = 2;
     [SerializeField] float driftFactor = .95f;
 
@@ -27,7 +27,6 @@ public class CarController : MonoBehaviour
     void FixedUpdate()
     {
         ApplyEngineForce();
-        KillLateralVelocity();
         ApplySteering();
     }
 
@@ -51,17 +50,6 @@ public class CarController : MonoBehaviour
 
     void ApplyEngineForce()
     {
-
-        if (accelerationInput == 0)
-        {
-
-            carRigidbody2D.drag = Mathf.Lerp(carRigidbody2D.drag, 2, Time.fixedDeltaTime * 3);
-        }
-        else
-        {
-
-            carRigidbody2D.drag = 0.1f;
-        }
 
         //Caculate how much "forward" we are going in terms of the direction of our velocity
 
@@ -88,12 +76,5 @@ public class CarController : MonoBehaviour
 
         //Apply steering by rotating the car object
         carRigidbody2D.MoveRotation(rotationAngle);
-    }
-
-    void KillLateralVelocity()
-    {
-        Vector2 forwardVelocity = transform.up * Vector2.Dot(carRigidbody2D.velocity, transform.up);
-        Vector2 lateralVelocity = transform.right * Vector2.Dot(carRigidbody2D.velocity, transform.right);
-        carRigidbody2D.velocity = forwardVelocity * lateralVelocity * driftFactor;
     }
 }
