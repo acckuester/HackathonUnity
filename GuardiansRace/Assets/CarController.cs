1e2d8ec98697779233175e419319447838745e5a
andreas kuester <andreas.kuester@ksb.com>
2024-03-08 20:16:44 +0100

    add border

--- GuardiansRace/Assets/CarController.cs
+++ GuardiansRace/Assets/CarController.cs
@@ -31,12 +31,22 @@ public class CarController : MonoBehaviour
         ApplySteering();
     }
 
+    // Gets called at the start of the collision 
     void OnCollisionEnter(Collision collision)
     {
-        if (collision.gameObject.tag == "Player")
-        {
-            Debug.Log("Entered collision with " + collision.gameObject.name);
-        }
+        Debug.Log("Entered collision with " + collision.gameObject.name);
+    }
+
+    // Gets called during the collision
+    void OnCollisionStay(Collision collision)
+    {
+        Debug.Log("Colliding with " + collision.gameObject.name);
+    }
+
+    // Gets called when the object exits the collision
+    void OnCollisionExit(Collision collision)
+    {
+        Debug.Log("Exited collision with " + collision.gameObject.name);
     }
 
     void ApplyEngineForce()
